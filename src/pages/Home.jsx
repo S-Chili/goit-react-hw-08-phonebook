0805bf5cab10b51fa8749da5261fb7cffa7aa3f2
Anastasiia <schili@outlook.com>
2023-04-12 16:02:10 +0300

    some trbls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import HowToRegTwoToneIcon from '@mui/icons-material/HowToRegTwoTone';
 import LoginTwoToneIcon from '@mui/icons-material/LoginTwoTone';
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
