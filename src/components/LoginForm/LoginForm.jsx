ce755478b94197fcf273be190ae40833541b12d8
Anastasiia <schili@outlook.com>
2023-04-12 15:29:43 +0300

    step 3

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,10 @@
-import { Field, Form, Input, PrimaryButton } from 'components/Shared.styled';
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operation';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
@@ -17,18 +21,48 @@ const LoginForm = () => {
     );
   };
 
+
+  const SubmitButton = styled(Button)({
+    margin: '1rem 0 0',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: '3px',
+    border: 0,
+    color: 'white',
+    height: '48px',
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  });
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Field>
-        Email:
-        <Input type="email" name="email" />
-      </Field>
-      <Field>
-        Password:
-        <Input type="password" name="password" />
-      </Field>
-      <PrimaryButton>Login</PrimaryButton>
-    </Form>
+    <form onSubmit={handleSubmit}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <TextField
+          id="input-with-icon-textfield"
+          name="email"
+          label="Email"
+          type="email"
+          InputProps={{
+            startAdornment: (
+              <AccountCircle />
+            ),
+          }}
+          sx={{ marginBottom: '20px' }}
+        />
+        <TextField
+          id="input-with-icon-textfield"
+          name="password"
+          label="Password"
+          type="password"
+          InputProps={{
+            startAdornment: (
+              <AccountCircle />
+            ),
+          }}
+          sx={{ marginBottom: '20px' }}
+        />
+        <SubmitButton type="submit">Login</SubmitButton>
+      </Box>
+    </form>
   );
 };
 
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,12 @@
-import { Field, Form, Input, PrimaryButton } from 'components/Shared.styled';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operation';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MailOutlineSharpIcon from '@mui/icons-material/MailOutlineSharp';
+import VpnKeySharpIcon from '@mui/icons-material/VpnKeySharp';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
@@ -18,22 +24,59 @@ const RegisterForm = () => {
     );
   };
 
+  const SubmitButton = styled(Button)({
+    margin: '1rem 0 0',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: '3px',
+    border: 0,
+    color: 'white',
+    height: '48px',
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  });
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Field>
-        Name:
-        <Input type="text" name="name" />
-      </Field>
-      <Field>
-        Email:
-        <Input type="email" name="email" />
-      </Field>
-      <Field>
-        Password:
-        <Input type="password" name="password" />
-      </Field>
-      <PrimaryButton>Register</PrimaryButton>
-    </Form>
+   <form onSubmit={handleSubmit}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <TextField
+          id="input-with-icon-textfield"
+          name="name"
+          label="Name"
+          type="name"
+          InputProps={{
+            startAdornment: (
+              <AccountCircle />
+            ),
+          }}
+          sx={{ marginBottom: '20px' }}
+        />
+         <TextField
+          id="input-with-icon-textfield"
+          name="email"
+          label="Email"
+          type="email"
+          InputProps={{
+            startAdornment: (
+              <MailOutlineSharpIcon />
+            ),
+          }}
+          sx={{ marginBottom: '20px' }}
+        />
+        <TextField
+          id="input-with-icon-textfield"
+          name="password"
+          label="Password"
+          type="password"
+          InputProps={{
+            startAdornment: (
+              <VpnKeySharpIcon />
+            ),
+          }}
+          sx={{ marginBottom: '20px' }}
+        />
+        <SubmitButton type="submit">Login</SubmitButton>
+      </Box>
+    </form>
   );
 };
 
